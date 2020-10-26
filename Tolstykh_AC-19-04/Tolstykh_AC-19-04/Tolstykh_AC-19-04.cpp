e89fd52ac62bb0750ef73ae89155d9f702ef93b5
ViktoriyaTolstyh <71029175+ViktoriyaTolstych@users.noreply.github.com>
2020-10-26 22:17:30 +0300

    Update Tolstykh_AC-19-04.cpp
    struct, create

--- Tolstykh_AC-19-04/Tolstykh_AC-19-04/Tolstykh_AC-19-04.cpp
+++ Tolstykh_AC-19-04/Tolstykh_AC-19-04/Tolstykh_AC-19-04.cpp
@@ -1,34 +1,85 @@
 ﻿#include <iostream> 
+#include <fstream>
 using namespace std;
 struct pipe
 {
-	string identificator;
+	int identificator;
 	float dlina;
-	string diametr;
+	float diametr;
 	bool priznak;
 
 };
 struct KC
 {
-	string identificator;
+	int identificator ;
 	string name;
-	int manufactory;
+ int kolvo_tsehov;
+ int kolvo_tsehov_v_rabote;
+ float effektivnost;
+};
+
+pipe createpipe ()
+{
+	pipe p;
+	p.identificator = 100;
+	cout << "Введите пожалуйста длину трубы : " << endl ;
+	cin >> p.dlina ; 
+	cout << "Введите пожалуйста диаметр трубы" << endl;
+	cin >> p.diametr;
+	cout << "Введите пожалуйста признак трубы" << endl;
+	cin >> p.priznak;
+	return p;
+};
+KC createKC()
+{
+	KC k;
+	k.identificator = 100;
+	cout << "Введите пожалуйста наименование КС : " << endl;
+	cin >> k.name;
+	cout << "Введите пожалуйста количество цехов" << endl;
+	cin >> k.kolvo_tsehov;
+	cout << "Введите пожалуйста количество цехов в работе" << endl;
+	cin >> k.kolvo_tsehov_v_rabote;
+	cout << "Введите пожалуйста значение эффективности КС" << endl ;
+	cin >> k.effektivnost;
+	return k;
+	
 
 };
-int createpipe (string name)
-{
-	pipe name
-	float dlina;
-	string diametr;
-	cin >> dlina ; 
-	cout << dlina;
-
-	cin >> diametr;
-	cout << diametr;
-};
-int main()
-{
-	pipe.score;
-}
-
+//pipe InputPipe()
+//{
+//	pipe p;
+//	cout << "Type priznak:";
+//	cin >> p.priznak ;
+//	return p;
+//}
+//void printpipe(pipe p)
+//{
+//	cout << "Priznak:" << p.priznak
+//		<< ""<<p.
+//}
+//int createKC(string name)
+//{
+//
+//}
+//void Editpipe(pipe p)
+//{
+//
+//}
+//void Savepipe(const pi & p)
+//{
+//	ofstream fout;
+//	fout.open("data.txt", ios::out);
+//	fout << p.name
+//}
+//int main()
+//{
+//	pipe p= InputPipe();
+//	printpipe(p);
+//	Editpipe(p);
+//	printpipe(p);
+//
+//	return 0;
+//}
+//
 
