1226d46192d16ccea8b923a7d3cfa73dbedabb0b
ViktoriyaTolstyh <71029175+ViktoriyaTolstych@users.noreply.github.com>
2020-12-09 14:18:24 +0300

    лр2 итог

--- Tolstykh_AC-19-04/Tolstykh_AC-19-04/Tolstykh_AC-19-04.cpp
+++ Tolstykh_AC-19-04/Tolstykh_AC-19-04/Tolstykh_AC-19-04.cpp
@@ -22,7 +22,6 @@ std::istream& operator>>(std::istream& in, CKC& cs)
 	cout << "Пожалуйста введите имя КС n - ";
 	cin.ignore();
 	getline(cin, cs.name);
-
 	cout << "Введите количество цехов - ";
 	cs.kolvo_tsehov = proverka(0, 1000);
 	cout << "Введите количесво цехов в работе - ";
@@ -58,7 +57,7 @@ void menu()
 
 }
 template<typename C>
-ostream& operator << (ostream& out, const vector<C>& object)// non pc
+ostream& operator << (ostream& out, const vector<C>& object)
 {
 	for (const C& i : object)
 	{
@@ -91,7 +90,7 @@ string checkRepair(Cpipe& p)
 }
 void EditAllPipes(vector<Cpipe>& pipes)
 {
-	cout << "0. Труба в работе\n1. Труба не в работе\nChoose - ";
+	cout << "0. Труба в работе\n1. Труба не в работе\nВыберите - ";
 	int choice = proverka(0, 1);
 	cout << endl;
 	for (Cpipe& i : pipes)
@@ -105,7 +104,7 @@ vector<Cpipe> EditOnePipe(vector<Cpipe>& pipes)
 	cout << "Введите id который вы хотите изменить: ";
 	int k;
 	cin >> k;
-	cout << "0.Труба в работе \n1. Труба не в работе\nChoose - ";
+	cout << "0.Труба в работе \n1. Труба не в работе\nВыберите - ";
 	int choice = proverka(0, 1);
 	pipes[k].priznak = choice;
 	cout << endl;
@@ -113,7 +112,7 @@ vector<Cpipe> EditOnePipe(vector<Cpipe>& pipes)
 }
 void EditPipe(vector<Cpipe>& pipes)
 {
-	cout << "1. Изменить все существующие трубы\n2. Изменить одну трубу\nSelect - ";
+	cout << "1. Изменить все существующие трубы\n2. Изменить одну трубу\nВыберите - ";
 	if (proverka(1, 2) == 1)
 	{
 		cout << endl;
@@ -128,7 +127,7 @@ void EditPipe(vector<Cpipe>& pipes)
 
 vector<CKC> EditAllKC(vector<CKC>& cs)
 {
-	cout << "\n0. Запустить цеха\n1. Остановить цеха\nSelect - ";
+	cout << "\n0. Запустить цеха\n1. Остановить цеха\nВыберите - ";
 	int choice = proverka(0, 1);
 	cout << endl;
 	for (CKC& i : cs)
@@ -164,7 +163,7 @@ vector<CKC> EditOneKC(vector<CKC>& cs)
 }
 void EditCS(vector<CKC>& cs)
 {
-	cout << "1. Изменить все существующие КС\n2. Изменить одну КС\nSelect - ";
+	cout << "1. Изменить все существующие КС\n2. Изменить одну КС\nВыберите - ";
 	if (proverka(1, 2) == 1)
 	{
 		cout << endl;
@@ -179,7 +178,7 @@ void EditCS(vector<CKC>& cs)
 }
 void Viewall(vector<Cpipe>&pipes, vector<CKC>&c)
 {
-	cout << "1. Просмотр всего\n" << "2. Просмотр труб\n" << "3. Просмотр КС\nSelect - ";
+	cout << "1. Просмотр всего\n" << "2. Просмотр труб\n" << "3. Просмотр КС\nВыберите - ";
 	switch (proverka(1, 3))
 	{
 	case 1:
@@ -337,7 +336,7 @@ void FiltrationCs(vector<CKC>& vect, bool(*f)(CKC& p, N param), N param)
 }
 void SearchByFilterPipes(vector<Cpipe>& pipes)
 {
-	cout << "\n1. По ID\n2. По состоянию\nSelect action - ";
+	cout << "\n1. По ID\n2. По состоянию\nВыберите действие - ";
 	if (proverka(1, 2) == 1)
 	{
 		cout << "Введите ID: ";
@@ -346,7 +345,7 @@ void SearchByFilterPipes(vector<Cpipe>& pipes)
 	}
 	else
 	{
-		cout << "\n1. В работе\n2. Не в работе\nSelect action - ";
+		cout << "\n1. В работе\n2. Не в работе\nВыберите действие - ";
 		int choice = proverka(1, 2);
 		FiltrationPipes(pipes, SearchByRepair, choice);
 	}
@@ -463,338 +462,4 @@ int main()
 	}
 	return 0;
 }
-//pipe createpipe ()
-//{
-//	pipe p;
-//	p.is_pipe == true;
-//	p.identificator = 100;
-//	p.priznak = proverka (0,1, "\n'Введите пожалуйста значения признака (в ремонте) : ");
-//	p.dlina = proverka(1, 1000, "\n'Введите пожалуйста длину трубы(>0) : ");
-//   p.diametr= proverka(1, 1000, "\n'Введите пожалуйста диамтер трубы(>0) : ");
-//	return p;
-//};
-//KC createKC()
-//{
-//	KC k;
-//	k.is_KC == true;
-//	k.identificator = 100;
-//	cout << "Введите пожалуйста наименование КС : " << endl;
-//	getline(cin, k.name);
-//	cin >> k.name;
-//	 k.effektivnost= proverka(1,100,"\'Введите пожалуйста значение эффективности:");
-//	k.kolvo_tsehov=proverka(1,10000, "Введите пожалуйста количество цехов(>=1)");
-//	 k.kolvo_tsehov_v_rabote=proverka(1,k.kolvo_tsehov, "Введите пожалуйста количество цехов в работе");
-//
-//   return k;	
-//};
-//void Viewpipe(const pipe& p)
-//{
-//	
-//		cout << "\nidentificator:" << p.identificator << endl;
-//		cout << "dlina:" << p.dlina << endl;
-//		cout << "diametr:" << p.diametr << endl;
-//		cout << "priznak:" << p.priznak << endl;
-//	}
-//void ViewKC(const KC& k)
-//{
-//		cout << "\nidentificator:" << k.identificator << endl;
-//		cout << "name:" << k.name << endl;
-//		cout << "kolvo_tsehov:" << k.kolvo_tsehov << endl;
-//		cout << "kolvo_tsehov_v_rabote:" << k.kolvo_tsehov_v_rabote << endl;
-//		cout << "effektivnost:" << k.effektivnost << endl;
-//}
-//void viewall(int v, const pipe& pi,const KC & st)
-//{
-//	switch (v)
-//	{
-//	case 1:
-//		Viewpipe(pi);
-//		break;
-//	case 2:
-//		ViewKC(st);
-//		break;
-//	}
-//	
-//}
-//
-//void Editpipe(pipe& p)
-//
-//{
-//	if (p.priznak == 1)
-//	{
-//		p.priznak = 0;
-//		cout << "\nТруба не в ремонте";
-//	}
-//	else
-//	{
-//		p.priznak = 1;
-//		cout << "\nТруба в ремонте";
-//	}
-//};
-//
-//void EditKC(KC& k)
-//{
-//	cout << "\n1.Цех в работе";
-//	cout << "\n2.Цех не в работе";
-//	cout << "\n3.Необходимо выбрать:";
-//	int choice;
-//	cin >> choice;
-//	do {
-//		if (choice == 1)
-//		{
-//			k.kolvo_tsehov_v_rabote += 1;
-//		}
-//		else
-//		{
-//			k.kolvo_tsehov_v_rabote -= 1;
-//		}
-//	} while (cin.fail() || isNumber(int(choice)));
-//
-//}; 
-//void Savepipe(const pipe& p)
-//{
-//	ofstream fout;
-//	fout.open("datapipe.txt", ios::out);
-//	if (fout.is_open())
-//	{
-//
-//		fout << "Identificator:" << p.identificator << endl;
-//		fout << "Dlina:" << p.dlina << endl;
-//		fout << "Diametr" << p.diametr << endl;
-//		fout << "Priznak" << p.priznak << endl;
-//		fout.close();
-//	}
-//}
-//void SaveKC(const KC& k)
-//{
-//	ofstream fout;
-//	fout.open("dataKC.txt", ios::out);
-//	if (fout.is_open())
-//	{
-//		fout << "Identificator:" << k.identificator << endl;
-//		fout << "Name:" << k.name << endl;
-//		fout << "Kolvo tsehov:" << k.kolvo_tsehov << endl;
-//		fout << "Kolvo tsehov v rabote:" << k.kolvo_tsehov_v_rabote << endl;
-//		fout << "Effektivnost:" << k.effektivnost << endl;
-//		fout.close();
-//	}
-//}
-//void save(const pipe& p, const KC& k)
-//{
-//	ofstream fout;
-//	fout.open("dataall.txt", ios::out);
-//	if (fout.is_open())
-//	{
-//		fout << "Identificator:" << p.identificator << endl;
-//		fout << "Dlina:" << p.dlina << endl;
-//		fout << "Diametr" << p.diametr << endl;
-//		fout << "Priznak" << p.priznak << endl;
-//		fout << "Identificator:" << k.identificator << endl;
-//		fout << "Dlina:" << p.dlina << endl;
-//		fout << "Diametr" << p.diametr << endl;
-//		fout << "Priznak" << p.priznak << endl;
-//		fout.close();
-//	}
-//
-//	}
-//void saveall(int s, const pipe& pi, const KC& st)
-//{
-//	switch (s)
-//	{
-//	case 1:
-//		Savepipe(pi);
-//		break;
-//	case 2:
-//		SaveKC(st);
-//		break;
-//	case 3:
-//		save(pi, st);
-//	}
-//}
-//
-//pipe Loadpipe()
-//     {pipe p;
-//		ifstream fin;
-//		fin.open("datapipe.txt", ios::in);
-//		if (fin.is_open())
-//		{
-//			fin >> p.dlina;
-//			fin >> p.diametr;
-//			fin >> p.priznak;
-//			fin.close();
-//			return p;
-//		}
-//	};
-//
-//KC LoadKC()
-//{
-//	KC k;
-//	ifstream fin;
-//	fin.open("dataKC.txt", ios::in);
-//	if (fin.is_open())
-//	{
-//		fin >> k.name;
-//		fin >> k.kolvo_tsehov;
-//		fin >> k.kolvo_tsehov_v_rabote;
-//		fin >> k.effektivnost;
-//		fin.close();
-//		return k;
-//	}
-//}
-//void Load(pipe& p, KC& k)
-//{
-//	ifstream fin;
-//	fin.open("dataall.txt", ios::in);
-//	if (fin.is_open())
-//	{
-//		fin >> p.dlina;
-//		fin >> p.diametr;
-//		fin >> p.priznak;
-//		fin >> k.name;
-//		fin >> k.kolvo_tsehov;
-//		fin >> k.kolvo_tsehov_v_rabote;
-//		fin >> k.effektivnost;
-//	}
-//	fin.close();
-//}
-//
-//void loadall(pipe&p, KC&k)
-//{
-//	switch (proverka(1,3,"\nВыберите 1-труба,2-КС,3-вместе :  "))
-//	{
-//	case 1:
-//		Viewpipe(Loadpipe());
-//		break;
-//	case 2:
-//		ViewKC(LoadKC());
-//		break;
-//	case 3:
-//		Load(p,k);
-//		break;
-//	}
-//}
-//
-//
-//	void PrintMenu()
-//{
-//	cout << "1.Труба" << endl
-//		<< "2.Компрессорная станция" << endl
-//		<< "3.Просмотреть" << endl
-//		<< "4.Редактирование трубы" << endl
-//		<< "5.Редактирование КС" << endl
-//		<<"6.Сохранить"<< endl
-//		<< "7.Загрузить"<< endl
-//		<< "0.Выйти" << endl;
-//}
-//	pipe& Selectpipe(vector<pipe>&g1)
-//	{
-//		unsigned int index = proverka(0u, g1.size(), "Введите индекс для трубы: ");
-//		return g1[index - 1];
-//	}
-//	KC& SelectKC(vector<KC>& g2)
-//	{
-//		unsigned int index = proverka(0u, g2.size(), "Введите индекс для КС: ");
-//		return g2[index - 1];
-//	}
-//
-//	int main()
-//	{
-//		setlocale(LC_ALL, "Russian");
-//		vector  <pipe> group1;
-//		vector <KC> group2;
-//		/*pipe pi;
-//		KC st;*/
-//		bool is_pipe = 0;
-//		bool is_KC =0;
-//		while (1)
-//		{
-//			PrintMenu();
-//
-//			switch (proverka(0, 7, "Меню:"))
-//			{
-//			case 1:
-//			{
-//				pipe pi;
-//				pi = createpipe();
-//				group1.push_back(pi);
-//				is_pipe == true;
-//				break;
-//			}
-//			case 2:
-//			{
-//				KC st;
-//				st = createKC();
-//				is_KC == true;
-//				break;
-//			}
-//		
-//			case 3:
-//				if (is_pipe == true && is_KC == true )
-//				{
-//					{
-//						cout << "Выберите пожалуйста нужный вариант. 1-pipe, 2- KC ";
-//						int v = 0;
-//						cin >> v;
-//						viewall(v, pi, st);
-//						break;
-//					}
-//				}
-//				else
-//				{
-//					cout << "Введите пожалуйста данные для пунктов 1,2";
-//				}
-//
-//			case 4:
-//			{
-//				if (is_pipe == true)
-//				{
-//					Editpipe(Selectpipe(group1));
-//					break;
-//				}
-//			}
-//			case 5:
-//			{
-//				if (is_KC == true)
-//				{
-//					EditKC(SelectKC(group2));
-//					break;
-//				}
-//			}
-//			case 6:
-//			{
-//				if (is_pipe == true && is_KC == true)
-//				{
-//					cout << "Выберите поджалуйста нужное :1-pipe, 2- KC, 3-all : ";
-//					int s = 0;
-//					cin >> s;
-//					for ();
-//					saveall(s, pi, st);
-//					break;
-//				}
-//	
-//			}
-//			case 7:
-//			{
-//				if (is_pipe == true && is_KC == true)
-//				{
-//				loadall(pi,st);
-//				break;
-//				}
-//				
-//			}
-//			case 0:
-//			{
-//				return 0;
-//			}
-//			default:
-//			{
-//				cout << "Возникла ошибка" << endl;
-//			}
-//
-//			}
-//		}
-//
-//		return 0;
-//	}
-//
-//
+
